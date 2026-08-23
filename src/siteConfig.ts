61dfe229f150296644d4a1bba9f0ef2df471e674
Fernando Valenzuela Jr <fernvalenjr@gmail.comr>
2026-08-22 23:59:37 -0400

    Add dynamic site and page-level under construction maintenance mode

---
+++ src/siteConfig.ts
@@ -0,0 +1,28 @@
+export interface MaintenanceConfig {
+  siteUnderConstruction: boolean;
+  pagesUnderConstruction: {
+    home: boolean;
+    about: boolean;
+    projects: boolean;
+    blog: boolean;
+    contact: boolean;
+  };
+  message: string;
+}
+
+export const maintenanceConfig = {
+  // Set to true to lock down the entire site
+  siteUnderConstruction: false,
+
+  // Enable/disable maintenance mode for specific pages
+  pagesUnderConstruction: {
+    home: false,
+    about: false,
+    projects: true,
+    blog: false,
+    contact: false,
+  },
+
+  // Custom messaging
+  message: "🚧 Under Construction: We're polishing this section. Check back soon!",
+};
